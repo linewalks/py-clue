63328e4bf73d333c7f0c3cff6ea06504d782b0d2
jungwoo <westhyena@gmail.com>
2022-02-11 10:12:01 +0900

    feat(setup): set for pypi upload

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
-from setuptools import setup, find_packages
-
-from pyclue import __version__ as version
+from setuptools import setup
 
 
 setup(
-    name="pyclue",
-    version=version,
-    description="Python interface to the MDwalks CLUE",
-    author="Linewalks",
-    author_email="jungwoo@linewalks.com",
-    url="https://github.com/linewalks/py-clue",
-    license="Linewalks",
-    python_requires=">=3.6",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     install_requires=[
         "grpcio",
         "protobuf"
     ],
     test_suite="tests",
     tests_require=["pytest"],
-    packages=find_packages(include=["pyclue", "pyclue.*"])
 )
