2c1c2caf24ec611c417b3cf444e323032228f5b2
westhyena <westhyena@gmail.com>
2022-12-01 13:20:08 +0900

    fix: remove REPOURL

--- bin/get_proto.default.sh
+++ bin/get_proto.default.sh
@@ -1,5 +1,5 @@
 # only linewalks members can access it.
-git clone https://khclab.kakaohealthcare.local/dp/clue-connect.git
+git clone REPO_URL
 mv clue-connect/clue_pb2.py ./pyclue/pb
 mv clue-connect/clue_pb2_grpc.py ./pyclue/pb
 rm -rf clue-connect
