be63700f6211297526798eb92d53e46d81dafce1
westhyena <westhyena@gmail.com>
2022-12-01 09:56:25 +0900

    feat(bin): mae default.sh to hide repo url

--- bin/get_proto.default.sh
+++ bin/get_proto.default.sh
@@ -1,5 +1,5 @@
 # only linewalks members can access it.
-git clone https://github.com/linewalks/clue-connect.git
+git clone https://khclab.kakaohealthcare.local/dp/clue-connect.git
 mv clue-connect/clue_pb2.py ./pyclue/pb
 mv clue-connect/clue_pb2_grpc.py ./pyclue/pb
 rm -rf clue-connect
